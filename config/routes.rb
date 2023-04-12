0357fe10b5d5dd8c8d3b37777cbc5cb94ab04e77
abdulhamiid <adioabdulhamid@yahoo.com>
2023-04-12 10:51:32 +0100

    Adjust page routing and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
+  get "/dashboard" => "dashboard#index"
   devise_for :users
 
   devise_scope :user do 
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  get 'home/index'
+  get '/home' => 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
