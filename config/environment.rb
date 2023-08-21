add7c9d1aa0b6b6ffa5599a4050a57ac2d0ac1fe
abdulhamiid <adioabdulhamid@yahoo.com>
2023-08-21 08:28:33 +0100

    Add confirmation mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.smtp_settings = {
+   :tls => true,
+   :address => "smtp.gmail.com",
+   :port => 587,
+   :domain => "gmail.com",
+   :authentication => :login,
+   :user_name => "abdulhameedsnr1@gmail.com",
+   :password => "rah3rah12345"
+ }
